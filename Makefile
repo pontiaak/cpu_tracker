02c6b3feda3cee1270fc0d00629785726c3b5f1b
pontiaak <pontiak.pontiak.2013@gmail.com>
2023-07-14 02:24:12 +0200

    updated make and auto test

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
 #This is a makefile for cpu tracker project by Igor Elche
+CC = gcc
+CFLAGS = -Wall -Wextra -lpthread
 
 final:
-	gcc tieto_cpu_tracker.c Reader.c Analyzer.c Printer.c Sigterm.c -o cpu_tracker_app -Wall -Wextra -lpthread
+	$(CC) tieto_cpu_tracker.c Reader.c Analyzer.c Printer.c Sigterm.c -o cpu_tracker_app $(CFLAGS)
+	
+test:
+	$(CC) unit_test.c Reader.c Analyzer.c Printer.c Sigterm.c -o auto_test_cpu_app $(CFLAGS)
+
+clean:
+	rm -rf cpu_tracker_app
+	rm -rf auto_test_cpu_app
---
+++ unit_test.c
@@ -0,0 +1,76 @@
+//UNIT test, it runs the whole program like tieto_cpu_tracker.c, but during runtime it tests the correctness of modules working
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>	//lib for threads
+#include <semaphore.h>	//lib for semaphores to use un producer-consumer-problem
+#include <signal.h>	//lib for handling signals
+#include "tieto_cpu_tracker.h"	//header file for this project
+#include <assert.h>	//lib for automatic tests done with the help of asserts
+#include <unistd.h>	//lib for sleep function
+
+struct cpuStatisticsStruct cpuStatistics[maximumCpuNumber];	//array of structures defined inheader file to be used as buffer-shared memmory between threads
+int cpuNumber = 0;	//number of cpus in shared memmory - cpuStatistics
+pthread_mutex_t mutexBuffer;	//mutex for producer-consumer problem solution
+sem_t producerSemaphore;	//creating a semaphore for reader thread (producer)
+sem_t consumerSemaphore;	//creating a semaphore for analyzer thread (consumer)
+unsigned long long cpuUsageSumm[maximumCpuNumber] = {0};	//array to act as buffer containing summ of multiple sets of cpu usage statistics of all processors to be later devided by cpuUsageSetsNumber during the printer thread to present avarage cpu usage for 1s
+int cpuUsageSetsNumber = -1;	//number of sets of data aggregated during 1second, starts with -1 because the first measurment set is alone and thus cannot be used to calculate percantage usage, we need to compare two sets
+
+volatile sig_atomic_t terminationRequest = 0;	//atomic flag, it is set to 1 to start closing the application gently
+
+int main()
+{	
+	if (signal(SIGTERM, SigtermHandler) == SIG_ERR)
+	{
+        perror("Error installing SIGTERM handler");
+        return EXIT_FAILURE;
+	}
+	pthread_t threadReader, threadAnalyzer, threadPrinter;
+	int retReader, retAnalyzer, retPrinter;	//values as output of creating threads to check for success in their creation
+	pthread_mutex_init(&mutexBuffer,NULL);	//initializing mutex buffer for pcp
+	sem_init(&producerSemaphore, 0, 1);
+	sem_init(&consumerSemaphore, 0, 0);
+	
+
+	retReader = pthread_create( &threadReader, NULL, ReaderTask, NULL);	//creating reader thread, second atribute is NULL, ReaderTask is a function this thread accomplishes
+	if (retReader != 0){printf("Problem encountered while creating Reader Thread\n"); return EXIT_FAILURE;}; /*else printf("Reader Thread Created Successfully\n");*/ 
+	retAnalyzer = pthread_create( &threadAnalyzer, NULL, AnalyzerTask, NULL);	//creating analyzer thread, second atribute is NULL, AnalyzerTask is a function this thread accomplishes
+	if (retAnalyzer != 0){printf("Problem encountered while creating Analyzer Thread\n"); return EXIT_FAILURE;}; /*else printf("Analyzer Thread Created Successfully\n");*/
+	retPrinter = pthread_create( &threadPrinter, NULL, PrinterTask, NULL);	//creatin printer thread, second atribute is NULL, PrinterTask is a function this thread accomplishes
+	if (retPrinter != 0){printf("Problem encountered while creating Printer Thread\n"); return EXIT_FAILURE;}; /*else printf("Printer Thread Created Successfully\n");*/ 
+	
+	sleep(2);	//sleeping for two seconds to let the threads copleat initial data read and exchange and not to do tests on empty memory 
+	while(!terminationRequest)//loop waiting for termination signal to exit, untill then repeats to let threads work
+	{
+		pthread_mutex_lock(&mutexBuffer);	//locking the tests into mutex beacause they cannot check perform memory result control during the brief moment that memory is being manipulated
+		
+		//testing the Reader thread for if it got correct data and CPU total statistics is greater or equal than first CPU core statistics (which it should because it is a total of all cores)
+		assert(
+		cpuStatistics[0].user >= cpuStatistics[1].user &&
+		cpuStatistics[0].nice >= cpuStatistics[1].nice &&
+		cpuStatistics[0].system >= cpuStatistics[1].system &&
+		cpuStatistics[0].idle >= cpuStatistics[1].idle &&
+		cpuStatistics[0].iowait >= cpuStatistics[1].iowait
+		);
+		
+		
+		pthread_mutex_unlock(&mutexBuffer);	//unlocking thread from mutex
+	}	
+	
+	pthread_join( threadReader, NULL);	//terminating thread
+	//printf("Reader Thread Terminated\n");	//checking for thread termination
+	
+	pthread_join( threadAnalyzer, NULL);	//terminating thread
+	//printf("Analyzer Thread Terminated\n");	//checking for thread termination
+
+	pthread_join( threadPrinter, NULL);	//terminating thread	
+	
+	//checking for thread termination
+	sem_destroy(&producerSemaphore);
+	sem_destroy(&consumerSemaphore);
+	system("clear");	//clearing screen for improved user interface
+	printf("Gracefully exited the program by sigterm handler\n");	
+	pthread_mutex_destroy(&mutexBuffer);	//destroing mutex buffer for pcp
+	return EXIT_SUCCESS;
+}
